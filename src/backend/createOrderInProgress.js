38ebce9998fd86e12a20293698d431de15542282
SebastianGuevara <sebastianguve@unisaba.edu.co>
2022-05-17 21:01:13 -0500

    Arreglos menores

--- src/backend/createOrderInProgress.js
+++ src/backend/createOrderInProgress.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 function createOrderInProgress(empresaP,empresaR)
 {
     let uuidO = uuidv4() 
-    db.collection8("orderInProgress").doc(uuidO).set({
+    db.collection("orderInProgress").doc(uuidO).set({
         id:uuidO,
         empresaPublication: empresaP,
         empresaRequest: empresaR
